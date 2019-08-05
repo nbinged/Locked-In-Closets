4565f37f798bcda2595a21e39c8d76f637aadebc
Nicholas Bingei <nbinged@gmail.com>
2019-08-05 21:39:55 +0800

    Commit 16:
    Add form done.

--- views/add.jsx
+++ views/add.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 var Default = require('./layouts/default');
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input, FormText, Card, CardBody, CardTitle, CardText, CardImg, ListGroup, ListGroupItem } from 'reactstrap';
 
 class add extends React.Component {
   render() {
@@ -9,43 +10,70 @@ class add extends React.Component {
     return (
 
     <Default>
-            <h1>New Clothes Form</h1>
-            <h3>Please enter the details of the item of clothing you would like to add to your archive.</h3>
+            <body>
 
-            <form enctype="multipart/form-data" action="/add" method="POST">
+                <Row>
+                    <Col className="add-bg">
+                    </Col>
+                </Row>
 
-                <input type="hidden" name="user_id" value={this.props.userID}/>
-
-                <input type="hidden" name="username" value={this.props.username}/>
-
-                <p>Item Name:</p>
-                <input type="text" name="item_name" required/>
-
-                <p>Brand:</p>
-                <input type="text" name="item_brand"/>
-
-                <p>Size:</p>
-                <input type="text" name="item_size"/>
-
-                <p>Color:</p>
-                <input type="text" name="item_color"/>
-
-                <p>Catergories:</p>
-                <input type="text" name="item_catergories" required/>
+            <Row>
+                <Col sm="12" md={{ size: 6, offset: 3 }}>
 
                 <br/>
+
+                <div className="request-text">
+                    <h1><strong>New Clothes Form</strong></h1>
+                    <h4>Please enter the details of the item <br></br> you would like to add to your collection</h4>
+                </div>
+
+        <Form enctype="multipart/form-data" action="/add" method="POST">
+
+            <input type="hidden" name="user_id" value={this.props.userID}/>
+
+            <input type="hidden" name="username" value={this.props.username}/>
+
+            <FormGroup>
+              <Label for="item-name"><strong>Name:</strong></Label>
+              <Input type="text" name="item_name" placeholder=" Himalaya Birkin bag" required/>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="item-brand"><strong>Brand:</strong></Label>
+              <Input type="text" name="item_brand" placeholder="Hermès" required/>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="item-size"><strong>Size:</strong></Label>
+              <Input type="text" name="item_size" placeholder="OS"/>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="item-color"><strong>Color:</strong></Label>
+              <Input type="text" name="item_color" placeholder="Smoky grey / Pearly White"/>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="item-catergories"><strong>Catergories:</strong></Label>
+              <Input type="text" name="item_catergories" placeholder="Bag"/>
+            </FormGroup>
+
                 <br/>
 
-                <p>Upload Your own picture</p>
-                  <input type="file" name="image_file"/>
+            <FormGroup>
+              <Label for="item-name"><strong>Upload your a picture of your item</strong></Label>
+              <Input type="file" name="image_file" placeholder="Birkin bag" required/>
+            </FormGroup>
 
                 <br/>
-                <br/>
 
-                <input type="submit" class="btn btn-primary"/>
+            <Button color="primary" input type="submit" size="lg" block>Submit</Button>
 
-            </form>
+            </Form>
 
+                    </Col>
+                </Row>
+            </body>
     </Default>
 
     );
